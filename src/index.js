8e2f81eee3dc4067d4de1c56e95df355bd6782a3
Gabriel Anderson <gabe4k@gmail.com>
2018-05-03 08:32:06 -0700

    Displays list of meds

--- src/index.js
+++ src/index.js
@@ -3,132 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css'
 
 
-class Timer extends React.Component {
-  constructor(props) {
-      super(props);
-      this.delay = delay;
-      this.state = { tick: 0, timestamp: Date.now() };
-
-      this.synchronizeWith = props.synchronizeWith;
-      this.synchronized = props.synchronizeWith !== undefined;
-
-      this.setTimeout = setTimeout;
-      this.stop = stop;
-      this.resume = resume;
-      this.setDelay = setDelay;
-  }
-
-  setTimeout() {
-      const { delay, synchronizeWith } = this;
-      const duration = delay - Math.abs(synchronizeWith - Date.now()) % delay;
-
-      this.timer = setTimeout(() => {
-          if (!this.stopped) this.setTimeout();
-          this.setState({
-              tick: this.state.tick + 1,
-              timestamp: Date.now()
-          });
-      }, duration);
-  }
-
-  start() {
-      this.stopped = false;
-      if (!this.synchronized) {
-          this.synchronizeWith = Date.now();
-      }
-      this.setTimeout();
-  }
-
-  resume() {
-      if (this.stopped) {
-          this.start();
-      }
-  }
-
-  stop() {
-      this.stopped = true;
-      clearTimeout(this.timer);
-  }
-
-  setDelay(delay) {
-      checkDelay(delay);
-      this.delay = delay;
-      if (!this.stopped) {
-          this.stop();
-          this.resume();
-      }
-  }
-
-  componentDidMount() {
-      this.start();
-  }
-
-  componentWillUnmount() {
-      this.stop();
-  }
-
-  render() {
-      const { props, delay, stop, resume, setDelay } = this;
-      const { tick, timestamp } = this.state;
-
-      const timer = { delay, tick, timestamp, stop, resume, setDelay };
-
-      return React.createElement(TimedComponent, { ...props, timer });
-  }
-}
-
-
-
-
 class PillOrg extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      schedule: [
+      schedule:
         {
-          time: '',
-          med: '',
-        }
-      ],
+          meds: [ {medName: 'Zrytec', medTime: '12:00PM'},
+                  {medName: 'Xanax', medTime: '12:00PM'},
+                  {medName: 'LSD', medTime: '3:00AM'}
+                ],
+        },
     };
   }
 
-  handleClick(i) {
-    const schedule = this.state.schedule;
-    const current = schedule[schedule.length - 1];
-    
-    this.setState({
-      schedule: schedule,
-    });
-  }
-
   render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
+    const meds = this.state.schedule.meds;
+    const medList = meds.map(function(med){
       return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
+        <div>
+          <li> {med.medName} at {med.medTime} </li>
+        </div>
       );
     });
 
     return (
       <div className="pillorg">
         <div className="pillorg-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+          <ol>{medList}</ol>
         </div>
         <div className="app-timer">
-          <Timer
-            squares={current.squares}
-            onClick={i => this.handleClick(i)}
-          />
         </div>
       </div>
     );
